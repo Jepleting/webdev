86b80933482c210c233e1031d7c0005f2ccaff36
Jepleting <jepletinglinah5@gmail.com>
2023-04-19 13:32:07 +0300

    Created Next.js app

---
+++ my-portofolio/pages/index.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Jepleting Portofolio</title>
+        <meta name="description" content=""></meta>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+       
+    <main></main>
+    </div>
+  )
+}
