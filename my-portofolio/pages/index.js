fc7e3f255e452e55592356d204e2282ba6850355
Jepleting <jepletinglinah5@gmail.com>
2023-04-19 19:02:01 +0300

    Created nav bar

--- my-portofolio/pages/index.js
+++ my-portofolio/pages/index.js
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import { Inter } from 'next/font/google';
+import {BsGithub} from 'react-icons/bs';
+
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +16,20 @@ export default function Home() {
       </Head>
 
        
-    <main></main>
+    <main className="bg-white px-10">
+      <section className="min-h-screen">
+        <nav className="py-10 mb-12 flex justify-between">
+          <h1 className="text-xl font-burtons">Jepleting Linah</h1>
+          <ul className="flex items-center">
+            <li >
+              <BsGithub className="cursor-pointer text-2xl"/>
+            </li>
+            <li><a className="bg-cyan-500 text-white px-4 py-2 rounded-md ml-8" href="#">Github Profile</a></li>
+          </ul>
+        </nav>
+        
+      </section>
+    </main>
     </div>
   )
 }
